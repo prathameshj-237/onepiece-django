1bf6bdf786ee043ccf288ad8ca0f75ce63f635d4
prathameshj-237 <jadhavprathamesh237@gmail.com>
2025-07-21 14:05:33 +0530

    reversed change of backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,4 @@
 terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=4.1.0"
-    }
-  }
-
   backend "azurerm" {
     resource_group_name  = "onepiece_project"
     storage_account_name = "onepieceterraformstorage"
